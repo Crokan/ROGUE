9720192c63a4eeea9ab9b178612773dea6afd45c
Crokan <crossroad6913@gmail.com>
2016-04-07 05:11:49 +0900

    json

---
+++ Assets/Scripts/CalenderParam.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace ROGUE
+{
+    public class Json_CalenderParam
+    {
+        public int year;
+        public int month;
+        public int clocksec;
+        public int ElpsMin;
+        public int ClockMin;
+    }
+
+    public class CalenderParam
+    {
+        public int Year;        // １年経つまでの月
+        public int Month;       // １月経つまでの日
+        public int clocksec;    // １クロックに必要なリアル秒
+        public int ElpsMin;     // １クロックで進むゲーム分
+        public int ClockMin;    // 時計に反映される分単位
+        
+        public void Desrialize(string json)
+        {
+            LitJson.JsonMapper.ToObject<
+        }
+    }
+}
---
+++ Assets/Scripts/JsonParse.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ROGUE
+{
+    public class JsonParse : MonoBehaviour {
+
+ 
+    }
+}
